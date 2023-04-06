22a7cb16286f3acb0ae6002a844aacb609b598c7
florianheck <florianheck0@gmail.com>
2023-04-06 12:44:28 +0200

    reformat

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -22,24 +22,24 @@
 #include <unistd.h>
 
 
-color ray_color(const ray& r, const hittable& world, int depth) {
+color ray_color(const ray &r, const hittable &world, int depth) {
     hit_record rec;
 
     // If we've exceeded the ray bounce limit, no more light is gathered.
     if (depth <= 0)
-        return color(0,0,0);
+        return color(0, 0, 0);
 
     if (world.hit(r, 0.001, infinity, rec)) {
         ray scattered;
         color attenuation;
         if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-            return attenuation * ray_color(scattered, world, depth-1);
-        return color(0,0,0);
+            return attenuation * ray_color(scattered, world, depth - 1);
+        return color(0, 0, 0);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 
@@ -47,12 +47,12 @@ hittable_list random_scene() {
     hittable_list world;
 
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
             if ((center - point3(4, 0.2, 0)).length() > 0.9) {
                 shared_ptr<material> sphere_material;
@@ -89,16 +89,16 @@ hittable_list random_scene() {
     return world;
 }
 
-void render_image(int image_height, int image_width, int samples_per_pixel, int max_depth, color* rendered_image,
+void render_image(int image_height, int image_width, int samples_per_pixel, int max_depth, color *rendered_image,
                   camera cam, hittable_list world, int max_parallelism, int n) {
 
-    for (int j = image_height-1-n; j >= 0; j -= max_parallelism) {
+    for (int j = image_height - 1 - n; j >= 0; j -= max_parallelism) {
         std::cerr << "\rScanlines remaining: " << j - n << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(0,0,0);
+            color pixel_color(0, 0, 0);
             for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
                 pixel_color += ray_color(r, world, max_depth);
             }
@@ -111,7 +111,7 @@ int main() {
 
     // Image
 
-    const auto aspect_ratio = 16.0 / 9.0;
+    const auto aspect_ratio = 16.0 / 10.0;
     const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 10;
@@ -123,9 +123,9 @@ int main() {
 
     // Camera
 
-    point3 lookfrom(13,2,3);
-    point3 lookat(0,0,0);
-    vec3 vup(0,1,0);
+    point3 lookfrom(13, 2, 3);
+    point3 lookat(0, 0, 0);
+    vec3 vup(0, 1, 0);
     auto dist_to_focus = 10.0;
     auto aperture = 0.1;
 
@@ -139,7 +139,7 @@ int main() {
 
     int image_size_in_bytes = sizeof(color) * image_width * image_height;
     auto *rendered_image = (color *) mmap(nullptr, image_size_in_bytes,
-           PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+                                          PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
     int status = 0;
 
